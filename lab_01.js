5ea16955f994254c132eee4a4a35bd27efde481a
Kingsu106 <boybensuli106@gmail.com>
2023-09-20 19:34:47 +0700

    Lab1-2

--- lab_01.js
+++ lab_01.js
@@ -1,2 +1,43 @@
+// Bai 1
+// Cau a
 console.log('Hello world')
+// Cau b
 console.log('LE QUANG THIEN')
+// Cau c
+console.clear() 
+// Cau d
+console.log('*****')
+console.log('*****')
+console.log('*****')
+console.log('*****')
+console.log('*****')
+// Cau e
+console.log('     *     ')
+console.log('    ***    ')
+console.log('   *****   ')
+console.log('  *******  ')
+console.log(' ********* ')
+console.log('***********')
+// Cau f
+console.log(' ** ** ')
+console.log('*  *  *')
+console.log(' *   * ')
+console.log('  * *  ')
+console.log('   *   ')
+console.clear()
+// Bai 2
+let a = 10
+let b = '10'
+// Cau a
+console.log(a)
+// Cau b
+console.log(b)
+// Cau c
+a = 5
+console.log(a)
+// Cau d
+const pi = 3.14
+console.log(pi)
+//  Cau e
+// pi = 5
+// console.log(pi)
