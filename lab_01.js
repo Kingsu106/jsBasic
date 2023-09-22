3d43d0409ebf97a011d1f537849c3a7ab7c7496b
Kingsu106 <boybensuli106@gmail.com>
2023-09-22 19:02:56 +0700

    Lab 1-3

--- lab_01.js
+++ lab_01.js
@@ -41,3 +41,84 @@ console.log(pi)
 //  Cau e
 // pi = 5
 // console.log(pi)
+// Cau f
+dienthoai = new Object()
+dienthoai.ten = 'Iphone 14'
+dienthoai.hangsanxuat = 'Apple'
+dienthoai.mausac = 'Trang'
+dienthoai.gia = '1000 USD'
+console.log(dienthoai)
+// Cau g
+dienthoai.gia = '750 USD'
+console.log(dienthoai)
+// Cau h
+dienthoai.ten = 'Iphone 14 Promax'
+console.log(dienthoai)
+console.clear()
+// Bai 3
+// Cau a
+a = 5
+b = 2
+console.log('a =',a)
+console.log('b =',b)
+console.log('Tong =',a + b)
+console.log('Hieu =',a - b)
+console.log('Tich =',a * b)
+console.log('Thuong =',a / b)
+// Cau b
+a = 9
+b = 7
+console.log('a =',a)
+console.log('b =',b)
+console.log('TBC =',(a+b)/2)
+// Cau c
+let x = 3
+let y = 4
+let z = 6
+console.log('x=',x)
+console.log('y=',y)
+console.log('z=',z)
+console.log('c1=',(x**2)+2*x+1)
+console.log('c2=',x**3-3*x*y-5*y+3*y**2)
+console.log('c3=',(x*y)**2-2*x**2*y+13*y)
+console.log('c4=',4*x**3+3*x*y+y**2-(2*x**2-3*y))
+console.log('c5=',((5*x**2)/(4*x*y))+y**2)
+console.log('c6=',x**2+(2*z*x*y)/(y**2)+5*x-2*y**2+4*x*z**3+z**3)
+// Cau d
+let d = 5
+console.log('Duong kinh =',d)
+console.log('Chu vi hinh tron =',d*pi)
+console.log('Dien tich hinh tron =',(d/2)**2*pi)
+// Cau e
+a = 15
+b = 10
+console.log('Chieu dai =',a)
+console.log('Chieu rong =',b)
+console.log('Chu vi hinh chu nhat =',(a+b)*2)
+console.log('Dien tich hinh chu nhat=',a*b)
+// Cau f
+d = 120
+let t = 130
+console.log('Quang duong =',d,'km')
+console.log('Thoi gian =',t,'min')
+console.log('Van toc=',d/(t/60),'km/h')
+// Cau g
+t = 150
+let v = 60
+console.log('Thoi gian =',t,'min')
+console.log('Van toc =',v,'km/h')
+console.log('QUang duong =',v*(t/60),'km')
+// Cau h
+v = 15.2
+t = 3
+console.log('Van toc =',v,'km/h')
+console.log('Thoi gian =',t,'hour')
+console.log('Quang duong =',v*t,'km')
+// Cau i
+t = (11*60) - ((8*60)+20)
+v = 42
+console.log('Van toc =',v,'km/h')
+console.log('Thoi gian =',t,'min')
+console.log('Quang duong =',v*(t/60),'km')
+// Cau j
+console.log('The tich =',(5*6*(8**3))+5*6*8,'cm^3')
